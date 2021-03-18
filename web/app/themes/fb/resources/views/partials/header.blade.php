6479d92b2b5113bd2c2e28fb73f3065d6842e5da
aitor <aitor@e451.net>
2021-03-18 20:05:46 +0100

    Banner desktop

--- web/app/themes/fb/resources/views/partials/header.blade.php
+++ web/app/themes/fb/resources/views/partials/header.blade.php
@@ -1,19 +1,19 @@
-<header class="banner flex">
+<header class="banner flex w-full">
   <nav class="w-full">
-    <ul class="flex justify-between">
-      <li class="li-brand inline-block p-4">
+    <ul class="flex">
+      <li class="li-brand inline-block sm:block p-4 sm:pt-8">
         <a class="brand nav-item hover:text-allo"" href="{{ home_url('/') }}">
           {{ $siteName }}
         </a>
       </li>
       @foreach ($primary_nav as $item)
-      <li class="li-shop  p-4">
+      <li class="li-shop p-4 sm:pt-8">
         <a href="{{ $item->url }}" class="nav-item hover:text-allo">{{ $item->label }}</a>
       </li>
       @endforeach
-      <li class="li-cart w-full ">
+      <li class="li-cart">
         <a href="/cart" class="cart-link flex nav-item hover:text-allo">
-          <div class="cart bg-white flex items-center flex-shrink flex-grow pl-8">
+          <div class="cart bg-white flex items-center flex-shrink flex-grow pl-8 sm:pr-8">
             <span>cart</span>
           </div>
           <div class="hole flex flex-col">
@@ -28,7 +28,7 @@
           <div class="espacio bg-white"></div>
         </a>
       </li>
-      <li id="btn-menu" class="li-menu block absolute">
+      <li id="btn-menu" class="li-menu block absolute sm:static sm:pt-6">
         <button class="btn block focus:rounded-none">MENU</button>
       </li>
     </ul>
