5dc2e25d0835bfd1cbeac79c91418a04679f7142
aitor <aitor@e451.net>
2021-03-21 15:05:47 +0100

    Montar slider. Fase 4( (enlace y datos de producto)

--- web/app/themes/fb/app/View/Composers/Slider.php
+++ web/app/themes/fb/app/View/Composers/Slider.php
@@ -47,10 +47,33 @@ class Slider extends Composer
             $img = get_field('img_producto', $post->ID);
             $formato = get_field('formato', $post->ID);
 
+            if ($formato == '50x70v') {
+                $formato_humano = '50 x 70 cm';
+            } elseif ($formato == '50x70h') {
+                $formato_humano = '70 x 50 cm';
+            } elseif ($formato == '61x91v') {
+                $formato_humano = '61 x 91 cm';
+            } elseif ($formato == '61x91h') {
+                $formato_humano = '91 x 61 cm';
+            }
+
+            $nombre = $post->post_title;
+            $permalink = get_permalink($post->ID);
+            $artist = get_the_terms($post->ID, 'artist')[0]->name;
+            $product = wc_get_product($post->ID);
+            $regular_price = $product->get_regular_price();
+
             return [
-               'img'     => $img,
-               'srcset'  => wp_get_attachment_image_srcset($img['ID']),
-               'formato' => $formato,
+               'img'            => $img,
+               'srcset'         => wp_get_attachment_image_srcset($img['ID']),
+               'formato'        => $formato,
+               'post'           => $post,
+               'product'        => $product,
+               'nombre'         => $nombre,
+               'url'            => $permalink,
+               'formato_humano' => $formato_humano,
+               'artist' => $artist,
+               'regular_price' => $regular_price,
             ];
         }, $posts);
 
--- web/app/themes/fb/resources/views/partials/slider.blade.php
+++ web/app/themes/fb/resources/views/partials/slider.blade.php
@@ -1,15 +1,23 @@
 @php $count = count($slider); @endphp
 
 <div class="flex relative justify-center bg-white">
-  <div id="fondo-slider" class="bg-black bg-cover w-100 md:w-3/4 absolute"></div>
+  <div id="fondo-slider" class="bg-cover w-100 md:w-3/4 absolute"></div>
   <div class="glide md:w-3/4">
     <div data-glide-el="track" class="glide__track">
       <ul class="glide__slides">
         @foreach ($slider as $slide)
         <li class="glide__slide" formato="{!! $slide['formato'] !!}">
-          <div class="prod w-100">
-            <img src="{!! $slide['img']['url'] !!}" srcset="{!! $slide['srcset'] !!}" sizes="(max-width: 640px) 100vw, 80vw", alt="">
-          </div>
+          <a href="{!! $slide['url'] !!}" class="prod relative w-100 block">
+            <div class="bg-hover bg-white top-0 bottom-0 absolute p-8 uppercase">
+              <div class="datos leading-tight">
+                <h2 class="text-black font-bold tracking-widest mb-4">{!! $slide['nombre'] !!}</h2>
+                <p class="text-black font-bold tracking-widest mb-4">{!! $slide['artist'] !!}</p>
+                <p class="text-black font-bold tracking-widest mb-4">{!! $slide['formato_humano'] !!}</p>
+                <p class="text-black font-bold tracking-widest mb-4">{!! $slide['regular_price'] !!} €</p>
+              </div>
+            </div>
+            <img class="relative" src="{!! $slide['img']['url'] !!}" srcset="{!! $slide['srcset'] !!}" sizes="(max-width: 640px) 100vw, 80vw", alt="">
+          </a>
         </li>
         @endforeach
       </ul>
@@ -40,3 +48,5 @@
 
   </div>
 </div>
+
+@dump($slider)
