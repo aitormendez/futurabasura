9ca01ff02210abfc1afc06e7bae992e6ce135720
aitor <aitor@e451.net>
2021-04-27 08:15:38 +0200

    añadir artist tax a REST API

--- web/app/themes/fb/app/filters.php
+++ web/app/themes/fb/app/filters.php
@@ -14,3 +14,31 @@ namespace App;
 add_filter('excerpt_more', function () {
     return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'sage'));
 });
+
+
+//Register taxonomy API for WC
+add_action( 'rest_api_init', function () {
+    register_rest_field('product', "artist", array(
+        'get_callback'    => __NAMESPACE__ . '\\product_get_callback',
+        // 'update_callback'    => __NAMESPACE__ . '\\product_update_callback',
+        'schema' => null,
+    ));
+} );
+
+
+
+//Get Taxonomy record in wc REST API
+function product_get_callback($post, $attr, $request, $object_type)
+{
+    $terms = array();
+
+    // Get terms
+    foreach (wp_get_post_terms( $post[ 'id' ], 'artist') as $term) {
+        $terms[] = array(
+            'id'        => $term->term_id,
+            'name'      => $term->name,
+            'slug'      => $term->slug,
+        );
+    }
+    return $terms;
+}
