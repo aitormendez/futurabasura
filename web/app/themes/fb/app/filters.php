064618dc4e5fdabb46e19112850213a67f2f9eff
aitor <aitor@e451.net>
2021-04-27 08:31:22 +0200

    eliminar completamente artist tax de REST API

--- web/app/themes/fb/app/filters.php
+++ web/app/themes/fb/app/filters.php
@@ -14,37 +14,3 @@ namespace App;
 add_filter('excerpt_more', function () {
     return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'sage'));
 });
-
-
-/**
- * Register taxonomy API for WC
- *
- * @link https://stackoverflow.com/a/62681137/2986401
- */
-add_action( 'rest_api_init', function () {
-    register_rest_field('product', "artist", array(
-        'get_callback'    => __NAMESPACE__ . '\\product_get_callback',
-        // 'update_callback'    => __NAMESPACE__ . '\\product_update_callback',
-        'schema' => null,
-    ));
-} );
-
-/**
- * Get Taxonomy record in wc REST API
- *
- * @link https://stackoverflow.com/a/62681137/2986401
- */
-function product_get_callback($post, $attr, $request, $object_type)
-{
-    $terms = array();
-
-    // Get terms
-    foreach (wp_get_post_terms( $post[ 'id' ], 'artist') as $term) {
-        $terms[] = array(
-            'id'        => $term->term_id,
-            'name'      => $term->name,
-            'slug'      => $term->slug,
-        );
-    }
-    return $terms;
-}
