d5eb75a77e6f03424288ee92c649b198d28d697c
aitor <aitor@e451.net>
2021-04-30 11:13:23 +0200

    columna artist avatar (no funciona)

--- web/app/themes/fb/app/filters.php
+++ web/app/themes/fb/app/filters.php
@@ -15,3 +15,25 @@ add_filter('excerpt_more', function () {
     return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'sage'));
 });
 
+
+
+add_filter('manage_artist_custom_column', function ($content,$column_name,$term_id){
+    $term= get_term($term_id, 'artist');
+    switch ($column_name) {
+        case 'artist_avatar':
+            $avatar = get_field('artist_avatar', $term);
+            // var_dump($avatar["url"]);
+            $content = '$avatar["url"]';
+            break;
+        default:
+            break;
+    }
+    return $content;
+},10,3);
+
+add_filter( 'manage_edit-artist_columns', function ( $columns ) {
+    $columns['artist_avatar'] = __('Artist Avatar', 'sage');
+    return $columns;
+} );
+
+
