af9c53450191f59e19ba88ae308faf782c7dc194
aitor <aitor@e451.net>
2021-05-01 19:16:36 +0200

    CSS página de artistas

--- web/app/themes/fb/resources/views/page-artists.blade.php
+++ web/app/themes/fb/resources/views/page-artists.blade.php
@@ -10,19 +10,20 @@
   @endwhile
 
   @foreach ($artists as $artist)
-    <a href="{{ $artist['permalink'] }}" role="article" class="text-black">
-      <h2 class="font-bold uppercase tracking-max">{{ $artist['name'] }}</h2>
-      <div class="bio text-sm">
-        {{ $artist['description'] }}
-      </div>
+    <a href="{{ $artist['permalink'] }}" role="article" class="article flex flex-wrap md:flex-nowrap p-6 md:pt-0 text-black bg-white mb-6 md:mb-2 justify-between">
+      <h2 class="font-bold uppercase tracking-max mb-6 md:pt-6 md:w-1/6 ">{{ $artist['name'] }}</h2>
 
-      <div class="avatar">
+      <div class="avatar flex items-center justify-center md:w-1/6 md:pt-6"">
         @if ($artist['avatar'])
-        <img src="{{ $artist['avatar']['url'] }}" srcset="{{ $artist['srcset'] }}" sizes="(max-width: 791px) 80vw, 20vw" alt="{{ $artist['name'] }}">
+        <img src="{{ $artist['avatar']['url'] }}" srcset="{{ $artist['srcset'] }}" sizes="(max-width: 791px) 80vw, 20vw" alt="{{ $artist['name'] }}" class="rounded-full">
         @endif
       </div>
 
-      <div class="productos">
+      <div class="bio text-sm mb-6 md:mb-0 md:px-6 md:w-1/3 md:pt-6 flex-grow">
+        {{ $artist['description'] }}
+      </div>
+
+      <div class="productos md:w-1/6 flex flex-wrap align-top">
         @foreach ($artist['products'] as $prod)
             {!! $prod['prod_img'] !!}
         @endforeach
