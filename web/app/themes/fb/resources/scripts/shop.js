ad265c34dc2081ddeefec80d919582e8a86343c0
aitor <aitor@e451.net>
2021-05-03 17:28:00 +0200

    Ajustes select2

--- web/app/themes/fb/resources/scripts/shop.js
+++ web/app/themes/fb/resources/scripts/shop.js
@@ -1,11 +1,13 @@
 $(document).ready(() => {
-  if (document.body.classList.contains('post-type-archive-product'))  {
+  if (document.body.classList.contains('post-type-archive-product') || document.body.classList.contains('tax-artist'))  {
 
       var select = '.dropdown_artist';
       function onProductTaxChange() {
           if ( $(select).val() !=='' ) {
               location.href = fb.homeUrl+ '/artists/'+$(select).val();
           }
+          console.log($(select).val());
+          console.log(select);
       }
       $(select).change( onProductTaxChange );
 
