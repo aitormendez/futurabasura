0dd0cf50810d226ebd7de7a461acd3131a0a9ffa
aitor <aitor@e451.net>
2021-05-15 18:20:36 +0200

    Ajustes CSS footer

--- web/app/themes/fb/resources/views/partials/footer.blade.php
+++ web/app/themes/fb/resources/views/partials/footer.blade.php
@@ -1,28 +1,28 @@
 <footer class="content-info pt-10 pb-2 flex flex-wrap items-center">
   <div class="footer-hole w-full flex justify-center mb-10">@svg('images.hole')</div>
-  <div class="menus flex flex-wrap w-full md:flex-nowrap md:items-start">
-    <ul class="contenidos w-full mx-3 my-2 px-3  py-2 border border-black hover:bg-white hover:bg-white">
+  <div class="menus flex flex-wrap w-full md:flex-nowrap md:items-start justify-center">
+    <ul class="contenidos w-full md:w-min mx-3 my-2 px-3  py-2 border border-black hover:bg-white hover:bg-white md:py-5 md:px-7">
       @foreach ($contents_nav as $item)
       <li class="">
         <a href="{{ $item->url }}" class="uppercase text-black tracking-widest text-sm hover:text-blue-700">{{ $item->label }}</a>
       </li>
       @endforeach
     </ul>
-    <ul class="shop w-full mx-3 my-2 px-3  py-2 border border-black hover:bg-white">
+    <ul class="shop w-full md:w-min mx-3 my-2 px-3 py-2 border border-black hover:bg-white md:py-5 md:px-7">
       @foreach ($shop_nav as $item)
       <li class="">
         <a href="{{ $item->url }}" class="uppercase text-black tracking-widest text-sm hover:text-blue-700">{{ $item->label }}</a>
       </li>
       @endforeach
     </ul>
-    <ul class="social w-full mx-3 my-2 px-3  py-2 border border-black hover:bg-white">
+    <ul class="social w-full md:w-min mx-3 my-2 px-3 py-2 border border-black hover:bg-white md:py-5 md:px-7">
       @foreach ($social_nav as $item)
       <li class="">
         <a href="{{ $item->url }}" class="uppercase text-black tracking-widest text-sm hover:text-blue-700">{{ $item->label }}</a>
       </li>
       @endforeach
     </ul>
-    <ul class="info w-full mx-3 my-2 px-3  py-2 border border-black hover:bg-white">
+    <ul class="info w-full md:w-min mx-3 my-2 px-3 py-2 border border-black hover:bg-white md:py-5 md:px-7">
       @foreach ($info_nav as $item)
       <li class="">
         <a href="{{ $item->url }}" class="uppercase text-black tracking-widest text-sm hover:text-blue-700">{{ $item->label }}</a>
