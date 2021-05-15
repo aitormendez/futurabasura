6b5a8b36a6c5515ee0fd07e147926df41d5403ec
aitor <aitor@e451.net>
2021-05-15 12:20:19 +0200

    Eliminar console log

--- web/app/themes/fb/resources/scripts/frontPage.js
+++ web/app/themes/fb/resources/scripts/frontPage.js
@@ -8,11 +8,8 @@ $(document).ready(() => {
     console.log(cupones);
 
     for (const c of cupones) {
-      console.log(c);
       altoOrg = c.offsetHeight;
-      console.log(altoOrg);
       alto = Math.ceil(altoOrg / 12) * 12;
-      console.log(alto);
       c.style.height = alto + 'px';
     }
 
