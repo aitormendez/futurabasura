2acb8f4fef61623a06e059674f84e7edb91d6851
aitor <aitor@e451.net>
2021-05-27 10:43:35 +0200

    Checkout. Coupon code

--- web/app/themes/fb/resources/views/woocommerce/checkout/form-checkout.blade.php
+++ web/app/themes/fb/resources/views/woocommerce/checkout/form-checkout.blade.php
@@ -1,4 +1,4 @@
-<?php
+@php
 /**
  * Checkout Form
  *
@@ -15,52 +15,51 @@
  * @version 3.5.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+ @endphp
+@if ( ! defined( 'ABSPATH' ) )
+	@php exit @endphp
+@endif
+<div class="w-full ticket-triangulo bg-tk-triangulo"></div>
+@php do_action( 'woocommerce_before_checkout_form', $checkout ); @endphp
 
-do_action( 'woocommerce_before_checkout_form', $checkout );
+{{-- If checkout registration is disabled and not logged in, the user cannot checkout. --}}
+@if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_required() && ! is_user_logged_in() )
+	{{ apply_filters( 'woocommerce_checkout_must_be_logged_in_message', __( 'You must be logged in to checkout.', 'woocommerce' ) ) }}
+	@php return @endphp
+@endif
 
-// If checkout registration is disabled and not logged in, the user cannot checkout.
-if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_required() && ! is_user_logged_in() ) {
-	echo esc_html( apply_filters( 'woocommerce_checkout_must_be_logged_in_message', __( 'You must be logged in to checkout.', 'woocommerce' ) ) );
-	return;
-}
+<form name="checkout" method="post" class="checkout woocommerce-checkout" action="{!! esc_url( wc_get_checkout_url() ) !!}" enctype="multipart/form-data">
 
-?>
+	@if ( $checkout->get_checkout_fields() )
 
-<form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
-
-	<?php if ( $checkout->get_checkout_fields() ) : ?>
-
-		<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
+		@php do_action( 'woocommerce_checkout_before_customer_details' ); @endphp
 
 		<div class="col2-set" id="customer_details">
 			<div class="col-1">
-				<?php do_action( 'woocommerce_checkout_billing' ); ?>
+				@php do_action( 'woocommerce_checkout_billing' ) @endphp
 			</div>
 
 			<div class="col-2">
-				<?php do_action( 'woocommerce_checkout_shipping' ); ?>
+				@php do_action( 'woocommerce_checkout_shipping' ) @endphp
 			</div>
 		</div>
 
-		<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
+		@php do_action( 'woocommerce_checkout_after_customer_details' ) @endphp
 
-	<?php endif; ?>
+	@endif
 
-	<?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
+	@php do_action( 'woocommerce_checkout_before_order_review_heading' ) @endphp
 
-	<h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
+	<h3 id="order_review_heading">{{ __( 'Your order', 'woocommerce' ) }}</h3>
 
-	<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
+	@php do_action( 'woocommerce_checkout_before_order_review' ) @endphp
 
 	<div id="order_review" class="woocommerce-checkout-review-order">
-		<?php do_action( 'woocommerce_checkout_order_review' ); ?>
+		@php do_action( 'woocommerce_checkout_order_review' ) @endphp
 	</div>
 
-	<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
+	@php do_action( 'woocommerce_checkout_after_order_review' ) @endphp
 
 </form>
 
-<?php do_action( 'woocommerce_after_checkout_form', $checkout ); ?>
+@php do_action( 'woocommerce_after_checkout_form', $checkout ) @endphp
