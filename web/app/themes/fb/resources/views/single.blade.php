d899ae5e9692bb1c0e3ae43b8a36ce70de2d3c6a
aitor <aitor@e451.net>
2021-06-09 12:17:09 +0200

    Correcciones Projects

--- web/app/themes/fb/resources/views/single.blade.php
+++ web/app/themes/fb/resources/views/single.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-  <main id="main" class="py-8 sm:mt-40 main">
+  <main id="main" class="py-8 main">
     @while(have_posts()) @php(the_post())
       @includeFirst(['partials.content-single-' . get_post_type(), 'partials.content-single'])
     @endwhile
