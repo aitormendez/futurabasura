8ff06a4586f4eaf2ef3f38f6491901bcc3c3e05c
aitor <aitor@e451.net>
2021-06-11 13:34:30 +0200

    Destacados. Formato mosaico front-end

--- web/app/themes/fb/resources/views/partials/destacados-portada.blade.php
+++ web/app/themes/fb/resources/views/partials/destacados-portada.blade.php
@@ -1,8 +1,9 @@
 @dump($destacado)
-<article class="mb-6">
-  <a href="{{ $destacado['link'] }}" class="flex flex-wrap w-full text-black bg-white md:justify-between md:flex-nowrap">
-    @if ($destacado['formato'] === 'imagen')
+<article class="mb-6 {{ $destacado['post_type'] }} {{ $destacado['formato'] }}">
 
+  {{-- IMAGEN --}}
+  @if ($destacado['formato'] === 'imagen')
+    <a href="{{ $destacado['link'] }}" class="flex flex-wrap w-full text-black bg-white md:justify-between md:flex-nowrap">
       <header class="w-full p-6 col-datos md:flex md:flex-col md:justify-between">
         <div class="arriba">
           <div class="font-serif text-lg font-bold capitalize meta">{{ $destacado['post_type'] }}</div>
@@ -12,12 +13,35 @@
           {!! $destacado['excerpt'] !!}
         </div>
       </header>
-      <div class="img">
-        <img src="{!! $destacado['url'] !!}" srcset="{!! $destacado['srcset'] !!}" alt="" sizes="">
-      </div>
+      @if ($destacado['has_img'])
+        <div class="img">
+          <img src="{!! $destacado['url'] !!}" srcset="{!! $destacado['srcset'] !!}" alt="" sizes="">
+        </div>
+      @endif
+    </a>
+  @endif
 
-    @endif
-  </a>
+  {{-- MOSAICO --}}
+  @if ($destacado['formato'] === 'mosaico')
+    <a href="{{ $destacado['link'] }}" class="flex flex-wrap w-full text-black bg-white md:justify-between md:flex-nowrap">
+      <header class="w-full p-6 col-datos md:flex md:flex-col md:justify-between">
+        <div class="arriba">
+          <div class="font-serif text-lg font-bold capitalize meta">{{ $destacado['post_type'] }}</div>
+          <h2 class="my-6 text-2xl tracking-widest">{{ $destacado['title'] }}</h2>
+        </div>
+        <div class="tracking-wide excerpt">
+          {!! $destacado['excerpt'] !!}
+        </div>
+      </header>
+      @if ($destacado['has_msc'])
+        <div class="flex flex-wrap items-start msc">
+          @foreach ($destacado['mosaico'] as $img)
+            <img class="w-2/4" src="{{ $img['url'] }}" alt="">
+          @endforeach
+        </div>
+      @endif
+    </a>
+  @endif
 
 </article>
 
