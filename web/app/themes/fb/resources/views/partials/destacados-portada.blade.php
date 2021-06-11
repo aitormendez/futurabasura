978bc97158f09be542e5f69167bb63874c7d4063
aitor <aitor@e451.net>
2021-06-11 12:34:12 +0200

    Destacados. Formato imagen front-end

--- web/app/themes/fb/resources/views/partials/destacados-portada.blade.php
+++ web/app/themes/fb/resources/views/partials/destacados-portada.blade.php
@@ -1,17 +1,23 @@
 @dump($destacado)
-<article>
+<article class="mb-6">
+  <a href="{{ $destacado['link'] }}" class="flex flex-wrap w-full text-black bg-white md:justify-between md:flex-nowrap">
+    @if ($destacado['formato'] === 'imagen')
 
-  @if ($destacado['formato'] === 'imagen')
+      <header class="w-full p-6 col-datos md:flex md:flex-col md:justify-between">
+        <div class="arriba">
+          <div class="font-serif text-lg font-bold capitalize meta">{{ $destacado['post_type'] }}</div>
+          <h2 class="my-6 text-2xl tracking-widest">{{ $destacado['title'] }}</h2>
+        </div>
+        <div class="tracking-wide excerpt">
+          {!! $destacado['excerpt'] !!}
+        </div>
+      </header>
+      <div class="img">
+        <img src="{!! $destacado['url'] !!}" srcset="{!! $destacado['srcset'] !!}" alt="" sizes="">
+      </div>
 
-  <header class="col-datos">
-    <div class="meta">{{ $destacado['post_type'] }}</div>
-    <h2>{{ $destacado['title'] }}</h2>
-    <div class="excerpt">
-      {!! $destacado['excerpt'] !!}
-    </div>
-  </header>
-
-  @endif
+    @endif
+  </a>
 
 </article>
 
