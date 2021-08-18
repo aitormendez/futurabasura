3e32b76a00e1522902dcba1f18f726c14ec0bd4d
aitor <aitor@e451.net>
2021-08-18 19:17:00 +0200

    Texto balanco destacados en hover

--- web/app/themes/fb/resources/views/partials/footer.blade.php
+++ web/app/themes/fb/resources/views/partials/footer.blade.php
@@ -30,14 +30,16 @@
       @endforeach
     </ul>
   </div>
-  <div class="flex flex-wrap justify-center w-full color">
-    <span class="mb-10 font-serif text-center frase-footer text-9xl" style="color: @option('footer_color')">@option('footer_frase')</span>
+
+  <div class="flex flex-wrap justify-center w-full mt-8 color">
+    <span class="mb-10 font-serif text-4xl text-center md:text-9xl sm:text-6xl frase-footer" style="color: @option('footer_color')">@option('footer_frase')</span>
     <div class="flex items-end w-full h-56 p-1 mancha" style="background-color: @option('footer_color')">
       <span class="text-xs uppercase">
         <a href="https://e451.net" class="text-black">451 web development</a>
       </span>
     </div>
   </div>
+
   <div class="w-full p-4 bg-white border border-black formulario">
     <div class="flex flex-wrap formu">
       <h3 class="tracking-widest">NEWSLETTER</h3>
