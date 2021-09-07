1dc1f6795ba61e08456ac7d453679eb55d430641
aitor <aitor@e451.net>
2021-09-07 15:56:05 +0200

    Arreglar formularios newsletter (ffoter y pag)

--- web/app/themes/fb/resources/views/partials/footer.blade.php
+++ web/app/themes/fb/resources/views/partials/footer.blade.php
@@ -40,9 +40,9 @@
     </div>
   </div>
 
-  <div class="w-full max-w-screen-md px-6 py-4 mx-auto bg-white border border-black formulario">
-    <div class="flex flex-wrap items-center sm:flex-nowrap formu">
-      <h3 class="tracking-widest sm:mr-10">NEWSLETTER</h3>
+  <div class="w-full max-w-screen-md px-4 mx-auto formulario">
+    <div class="flex flex-wrap items-center px-6 py-4 bg-white border border-black sm:flex-nowrap formu">
+      <h3 class="text-sm tracking-widest sm:mr-10">NEWSLETTER</h3>
       {!! do_shortcode( '[mc4wp_form id="474"]' ) !!}
     </div>
   </div>
