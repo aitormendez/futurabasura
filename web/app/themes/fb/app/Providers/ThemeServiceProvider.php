2ef39000d696e8773df04eb92a7a24dca1688d4e
aitor <aitor@e451.net>
2021-09-18 15:23:55 +0200

    Carlos Faria gist

--- web/app/themes/fb/app/Providers/ThemeServiceProvider.php
+++ web/app/themes/fb/app/Providers/ThemeServiceProvider.php
@@ -13,7 +13,31 @@ class ThemeServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->booted(
+            function($app) {
+                set_error_handler(function ($level, $message, $file = '', $line = 0, $context = []) {
+                    // Check if this error level is handled by error reporting
+                    if (error_reporting() & $level) {
+                        // Return false for any error levels that should
+                        // be handled by the built in PHP error handler.
+                        if ($level & (E_WARNING | E_NOTICE | E_DEPRECATED)) {
+
+                            echo "<br />\n";
+                            echo "<b>ERROR</b> [$level] $message<br />\n";
+                            echo "  Fatal error on line $line in file $file";
+                            echo ", PHP " . PHP_VERSION . " (" . PHP_OS . ")<br />\n";
+                            echo "Aborting...<br />\n";
+
+                            return false;
+                        }
+
+                        // Throw an exception to be handled by Laravel for all other errors.
+                        throw new \ErrorException($message, 0, $level, $file, $line);
+                    }
+                });
+            }
+        );
+
     }
 
     /**
@@ -23,6 +47,6 @@ class ThemeServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+
     }
 }
