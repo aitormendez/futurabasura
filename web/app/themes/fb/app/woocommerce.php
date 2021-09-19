1605e2f73a01d41a095f40fe6e9a7a002b4f6fab
aitor <aitor@e451.net>
2021-09-19 15:10:43 +0200

    Nota para el carrito

--- web/app/themes/fb/app/woocommerce.php
+++ web/app/themes/fb/app/woocommerce.php
@@ -161,3 +161,13 @@ function template_redirect_action() {
     unset($orderby["popularity"]);
     return $orderby;
 }, 20 );
+
+
+/**
+ * Eliminar reviews de single product y additional information tab.
+ */
+add_filter( 'woocommerce_after_shipping_calculator', function()  {
+    echo '<h1>' . get_field('exp_car_totals', 'option') . '</h1>';
+});
+
+
